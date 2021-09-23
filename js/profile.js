0c9d0a67878017c7380e790e072a9f11c60dbce2
sacden <sacden2009@gmail.com>
2021-09-23 12:17:59 +0200

    fix: change css styles for mobile devices

--- js/profile.js
+++ js/profile.js
@@ -2,7 +2,7 @@ const profileEl = document.querySelector("#profile");
 profileEl.insertAdjacentHTML(
   "afterbegin",
   `         <section class="about text-center">
-<div class="logo"><img src="./images/avatar.png" alt="Denis Shakhmaev" width="100" height="100" /></div>
+<div class="logo"><img src="./images/avatar.png" alt="Denis Shakhmaev" width="100" height="100"  /></div>
 
 <div class="name">
   <a href="" class="own-links"><h1>Denis Shakhmaev</h1></a>
